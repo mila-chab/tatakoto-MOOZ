49f1074246d61318c2bf8e17d04d5e2ae20bce2e
Mila Chabassier <mili25cha@gmail.com>
2026-05-21 14:40:20 -1000

    create retrieve.raw.Data function

---
+++ utils.R
@@ -0,0 +1,62 @@
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(stringr)
+
+retrieve.raw.data <- function(root_folder, folder_path, output_path, run, skip = 136, save_data = FALSE) {
+  path <- paste(root_folder, folder_path, sep = "/")
+  column_names <- c(
+      "Date",
+      "Time",
+      "Duration.s",
+      as.vector(rbind(paste0("Ox.", 1:8),
+                      paste0("Temp.", 1:8)))
+  )
+
+  files_list <- list.files(path = path,
+                           pattern = "*.txt")
+
+  file <- files_list[1]
+  table <- read.table(file = paste(path, file, sep = "/"), sep = "\t", header = TRUE, fill = TRUE,  skip = skip) |>
+    clean_column_names(column_names)
+
+  if (save_data) {
+    write.csv(table,
+              paste(output_path, "/", run, ".csv", sep = ""),
+              sep = ";", dec = ".", row.names = FALSE)
+  }
+}
+
+
+extract.metadata <- function(root_folder) {
+  metadata <- read.table(file = paste(root_folder, "metadata.csv", sep = ""),
+                         sep = ";", fill = TRUE, header = TRUE)
+  temp_metadata <- read.table(file = paste(root_folder, "temperature_metadata.csv", sep = ""),
+                              sep = ";", fill = TRUE, header = TRUE)
+
+  temp_metadata <- temp_metadata |>
+    mutate(Start_Time_Day = hms::as_hms(Start_Time_Day),
+           Close_Time_Day = hms::as_hms(Close_Time_Day),
+           Start_Time_Night = hms::as_hms(Start_Time_Night),
+           Close_Time_Night = hms::as_hms(Close_Time_Night),
+           Temperature = as.numeric(gsub(",", ".", Temperature.C)))
+
+  list(metadata = metadata, temp_metadata = temp_metadata)
+}
+
+clean_column_names <- function(df, column_names) {
+  columns_reference <- as.vector(
+        c(1:3, sapply(
+            0:3,
+            function(i) c(4 + 18*i, 12 + 18*i))
+        ,
+        sapply(
+            0:3,
+            function(i) c(4 + 18*i + 77, 12 + 18*i + 77)
+        ))
+    )
+
+  df <- df[, columns_reference]
+  colnames(df) <- column_names
+  df
+}
